5771f0d60240e9e523663bb978988dfea1201e65
Valentinaga1 <1881@holbertonschool.com>
2021-02-01 22:57:09 -0500

    Adding task3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,32 +2,24 @@
 """Using and API and work in Json
 """
 import json
-import requests
+from requests import get
 
 
 if __name__ == "__main__":
-    """working on an API
-    """
-    employee_id = "https://jsonplaceholder.typicode.com/users"
-    employe_id_response = requests.get(employee_id)
-    employe__id_response_dict = employe_id_response.json()
-    dict_to_json = {}
-    for user in employe__id_response_dict:
-        new_list = []
-        id = user.get('id')
-        username = user.get('username')
-
-        todos = "https://jsonplaceholder.typicode.com/todos?userId={}".format(id)
-        todos_response = requests.get(todos)
-        tasks_list = todos_response.json()
-
-        for item in tasks_list:
-            new_dict = {}
-            new_dict['username'] = username
-            new_dict['task'] = item.get('title')
-            new_dict['completed'] = item.get('completed')
-            new_list.append(new_dict)
-        dict_to_json["{}".format(id)] = new_list
-
-    with open('todo_all_employees.json', mode="w") as File_json:
-        json.dump(dict_to_json, File_json)
+    info = "https://jsonplaceholder.typicode.com/users/"
+    users = get(info).json()
+    totalU = [user for user in users]
+    d = {}
+    for userId in range(1, len(totalU) + 1):
+        name = get("https://jsonplaceholder.typicode.com/users/{}"
+                   .format(userId)).json()
+        tasksuser = get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                        .format(userId)).json()
+        dict_ = []
+        for task in tasksuser:
+            dict_.append({"task": task.get("title"),
+                         "completed": task.get("completed"),
+                          "username": name.get("username")})
+        d[userId] = dict_
+    with open('todo_all_employees.json', 'w') as f:
+        f.write(json.dumps(d, indent=4))
