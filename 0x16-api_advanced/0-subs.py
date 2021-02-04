317537fa65fe028881bdb6de7a461d01b0fc0641
Valentinaga1 <1881@holbertonschool.com>
2021-02-04 13:48:49 -0500

    first commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,9 @@ def number_of_subscribers(subreddit):
     """Function to returns the number of subscribers"""
 
     url = "https://www.reddit.com/r/"
+    headers = {"User-Agent": "User Agent"}
     response = requests.get(url + subreddit + "/about.json",
-                            allow_redirects=False)
+                            headers=headers, allow_redirects=False)
     # print(response)
     if response.status_code == 200:
         x = response.json().get("data").get("subscribers")
