36d3da7ff288448634e92b3d55678aa3c263bcf2
Valentinaga1 <1881@holbertonschool.com>
2021-02-04 13:43:12 -0500

    first commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and returns the number of
+subscribers"""
+
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function to returns the number of subscribers"""
+
+    url = "https://www.reddit.com/r/"
+    headers = {"User-Agent": "User Agent"}
+    response = requests.get(url + subreddit + "/about.json",
+                            headers=headers, allow_redirects=False)
+    # print(response)
+    if response.status_code == 200:
+        x = response.json().get("data").get("subscribers")
+        # print(response.json().get("data"))
+        return response.json().get("data").get("subscribers")
+    else:
+        return 0
